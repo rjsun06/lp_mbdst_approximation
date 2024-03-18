2686c1a3a1825c5beb7f0ccf7c384f4f4033a3bd
Renjie Sun <rjsun06@gmail.com>
2024-03-18 02:36:44 -0500

    verbosity

--- test.py
+++ test.py
@@ -4,27 +4,32 @@ import itertools
 from mbdst_kplus1 import MBDST1,MBDST2, MBDST3
 from utils import *
 
+_log = print
+def set_log(fun):
+    global _log
+    _log = fun
 #%%
 def test_single_MBDST(fun,N):
     case = gen_case(N)
-    # print(fun(*case))
-    return None
+    ret = fun(*case)
+    return ret
 
 def compare_results(funs,N):
     g,c,b = gen_case(N)
     ret = [f(g.copy(), c.copy(), b.copy()) for f in funs]
     for tmp in ret:
         if tmp is None and is_feasible(g,c,b):
-            print('got None')
-            print(ret)
-            print(c,b)
-            return
+            _log('got None')
+            _log(ret)
+            _log(c,b)
+            return ret,-1
     ret = np.stack(ret)
     if np.logical_or.reduce(ret != ret[0],axis=0).any():
-        print('got diff')
-        print(ret)
-        print(c,b)
-    return None
+        _log('got diff')
+        _log(ret)
+        _log(c,b)
+        return ret,1
+    return ret,0
 #%%
 def test():
     # test_single_MBDST(MBDST2,15)
